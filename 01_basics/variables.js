03ea57951d63c6d6922996fb46c6f51cf202db2b
Ashlesh-12 <ashleshpai12@gmail.com>
2025-03-24 14:27:11 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ashlesh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mangaluru"
+
+// accountId = 2 // not allowed\
+
+accountEmail = "hi.com"
+accountPassword = 5354633
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
