9af013d6ce00a104336ae7c8b983ccf9e10bba39
Ashlesh-12 <ashleshpai12@gmail.com>
2025-04-19 06:32:14 +0000

    new file

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,15 @@
+// Operations
+
+let value1 = 3
+let negvalue = -value1
+
+console.log(negvalue);
+
+let str1 = "Hello"
+let str2 = " I"
+let str3 = " am"
+let str4 = " Ashlesh"
+let str5 = str1 + str2 + str3 +str4
+console.log(str5);
+
+console.log(typeof("1" + 2));
